25f541a0a195917754a2c8f0f25bba59c224ea53
k5cents <k5cents@gmail.com>
2017-11-17 23:18:17 -0500

    Update start_tax.R

--- start_tax.R
+++ start_tax.R
@@ -1,5 +1,4 @@
 # This code formats the tax data set (eventually with region info).
-
 library("sp")
 library("maptools")
 library(readr)
@@ -13,9 +12,6 @@ nbh.shapes <- as.list(rep(NA, length(nbh.raw)))
 for(i in 1:length(nbh.raw)){
   nbh.shapes[[i]] <- Polygon(nbh.raw[[i]])
 }
-
-# Ensure the resulting list is polygons and formatted right.
-head(nbh.shapes, 1)
 
 # Read in a list of neighborhood names, in the same order as the shapes.
 nbh.names <- read.csv("https://raw.githubusercontent.com/k5cents/dc-bmp/master/names.csv", header = FALSE)$V1
@@ -31,7 +27,7 @@ for(i in 1:length(nbh.raw)){
 # Download the property addresses as a CSV file.
 prop_address_list <- tax$PROPERTY_ADDRESS
 prop_address_list <- as.data.frame(prop_address_list)
-write_excel_csv(prop_address_list, path = "/home/kiernan/govt310/prop_address_list.csv")
+write_excel_csv(prop_address_list, path = "/home/kiernan/Downloads/prop_address_list.csv")
 
 # Use the DC MAR to assign Latitude, Longitude to property addresses.
 tax.long <- read.csv("TAX_LONG.csv") # Created with MAR
@@ -51,7 +47,7 @@ for(i in 1:length(nbh.char)){
     (get(nbh.char[i]))@coords[,2])
 }
 
-# Create single variable indicating neighborhood for each BMP entry.
+# Create single variable indicating neighborhood for each property entry.
 tax$neighborhood <- NA
 for(i in 1:length(nbh.char)){
   tax$neighborhood[tax[nbh.char[i]] == 1] <- nbh.char[i]
@@ -61,5 +57,5 @@ for(i in 1:length(nbh.char)){
 tax[nbh.char[1:length(nbh.char)]] <- NULL
 
 # Save modified dataset
-write.csv(tax, "BMP.csv")
+write.csv(tax, "tax.csv")
 head(tax$neighborhood)
