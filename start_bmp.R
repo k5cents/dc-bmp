aac361610c528bc2839f0ac5284243a5842408a0
k5cents <k5cents@gmail.com>
2017-11-20 13:17:27 -0500

    Add tract information

--- start_bmp.R
+++ start_bmp.R
@@ -2,9 +2,11 @@
 library("sp")
 library("maptools")
 
-# Download the KML and BMP data.
+# Download the KML and CSV data.
 nbh.raw <- getKMLcoordinates(kmlfile="https://raw.githubusercontent.com/k5cents/dc-bmp/master/shapes.kml", ignoreAltitude = T)
+tract.raw <- getKMLcoordinates(kmlfile="https://opendata.arcgis.com/datasets/b78d164649ad487db0f2b20a9261fb8c_17.kml", ignoreAltitude = T)
 BMP <- read.csv("https://opendata.arcgis.com/datasets/a973c2c7b7c14a918859f3e38bdffdd2_42.csv")
+income <- read.csv("https://opendata.arcgis.com/datasets/b78d164649ad487db0f2b20a9261fb8c_17.csv")
 
 # Create empty list, then fill with neighborhood boundies as polygons.
 nbh.shapes <- as.list(rep(NA, length(nbh.raw)))
@@ -39,9 +41,46 @@ for(i in 1:length(nbh.char)){
   BMP$neighborhood[BMP[nbh.char[i]] == 1] <- nbh.char[i]
 }
 
-# Remove the neighborhood presence indicator variables
-BMP[nbh.char[1:length(nbh.char)]] <- NULL
+# Repeat this all with census tract info
 
-# Save modified dataset
+# Create empty list, then fill with tract boundies as polygons.
+tract.shapes <- as.list(rep(NA, length(tract.raw)))
+for(i in 1:length(tract.raw)){
+  tract.shapes[[i]] <- Polygon(tract.raw[[i]])
+}
+
+# Apply that list of tract "names" as the names for each neighborhood shape.
+tract.vec <- rep("tract", length(tract.raw))
+tract.nos <- tract.names <- as.character(read.csv("https://github.com/k5cents/dc-bmp/raw/master/fedtract.csv", header = F)$V1)
+tract.no <-paste(tract.vec, tract.nos, sep = "_")
+names(tract.shapes) <- tract.no
+
+# Turn the list of tract polygon coordinates into individual objects for each neighborhood.
+for(i in 1:length(tract.raw)){
+  assign(names(tract.shapes)[i], tract.shapes[[i]])
+}
+# Create variable indicating presence in each neighborhood.
+for(i in 1:length(tract.no)){
+  BMP[tract.no[i]] <- point.in.polygon(
+    BMP$LONGITUDE,
+    BMP$LATITUDE,
+    (get(tract.no[i]))@coords[,1],  
+    (get(tract.no[i]))@coords[,2])
+}
+
+# Create single variable indicating neighborhood for each BMP entry.
+BMP$tract <- NA
+for(i in 1:length(tract.no)){
+  BMP$tract[BMP[tract.no[i]] == 1] <- tract.no[i]
+}
+
+# Remove the presence indicator variables and extra objects
+BMP[nbh.char[1:length(nbh.char)]] <- NULL
+BMP[tract.no[1:length(tract.no)]] <- NULL
+rm(list = nbh.char)
+rm(list = tract.no)
+BMP$tract <- gsub("tract_", "", BMP$tract)
+
+# Ensure each variable is formatted correctly
+head(BMP$tract)
 head(BMP$neighborhood)
-write.csv(BMP, "BMP.csv")
